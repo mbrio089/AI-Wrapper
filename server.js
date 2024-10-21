c1dffc7062f1058634c23cb08c4817f3085016bf
mbrio <moritz.e.braun@gmail.com>
2024-10-21 23:21:27 +0200

    Server Prod Build

--- server.js
+++ server.js
@@ -127,5 +127,12 @@ app.use((req, res, next) => {
   next();
 });
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
